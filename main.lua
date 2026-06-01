d744da8419285ae46f6f34dd00136d6e9a2e7dde
6ase3rei <oprea.matei.daniel@gmail.com>
2026-06-01 22:42:46 +0300

    Delete main.lua

--- main.lua
+++
@@ -1,5 +0,0 @@
-if game.GameId == 7671049560 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/6ase3rei/Eclipse/refs/heads/main/theforge.lua"))()
-elseif game.GameId == 8144728961 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/6ase3rei/Eclipse/refs/heads/main/abyss.lua"))()
-end
